fac3f0545b912b0a28774c977c7847a757fa1df9
Nishank Mishra <115864595+nishankmishra@users.noreply.github.com>
2024-01-19 09:25:34 +0000

    19jan24

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,6 +1,6 @@
 function Search(){
     return(
-        <form className='w-1/3'>   
+        <form className='w-1/2'>   
             <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div class="relative ">
                 <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
