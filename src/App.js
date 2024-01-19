37b10f42e40584faacf3e3d706e783ebe2b57f17
Nishank Mishra <115864595+nishankmishra@users.noreply.github.com>
2024-01-19 07:52:59 +0000

    19jan24

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-
 import './App.css';
 
 function App() {
   return (
    <div className='bg-[#DEDEDC] w-full flex p-3 gap-5'>
 
-    <div className="w-1/5 h-[100vh] bg-[#798991] rounded-xl ">
-         <p className='text-8xl text-center mt-5 text-white'>RSA</p>
-         <div className='text-center mt-[100px] text-xl gap-5'>
-          <ul className='gap-5 '>
+    <div className="w-1/5 h-[96vh] bg-[#798991] rounded-xl ">
+        <p className='text-8xl text-center mt-5 text-white'>RAS</p>
+        <div className='text-center mt-[40px] text-xl gap-5'>
+          <ul className=' gap-8 cursor-pointer	'>
             <li className='py-3 text-white'>Dashboard</li>
-            <li className='py-3 text-white'>Dashboard</li>
-            <li className='py-3 text-white'>Dashboard</li>
-            <li className='py-3 text-white'>Dashboard</li>
+            <li className='py-3 text-white'>Accounting</li>
+            <li className='py-3 pr-4 text-white'>Purchase</li>
+            <li className='py-3 pr-11 text-white'>Sales</li>
+            <li className='py-3 pr-8 text-white'>Setting</li>
             </ul>
          </div>
     </div>
@@ -34,19 +34,19 @@ function App() {
             </div>
         </form>
 
-        <div className='w-2/3 justify-end items-center flex '>
-
-            <img class="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 object-cover" src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D" alt="Bordered avatar" />
-
+        <div className='w-2/3 justify-end items-center flex gap-2 text-xl cursor-pointer font-semibold'> User
+          
+            <img class="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 object-cover cursor-pointer	" src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D" alt="Bordered avatar" />
+            
           </div>
 
 
 
       </div>
       <div className=' w-full h-[10%] flex items-center'>
-          <h2 className='text-xl ml-3'>Dashboard / Stocks / Runner Bolt</h2>
+          <h2 className='text-2xl ml-3 cursor-pointer'>Dashboard / Stocks / Runner Bolt</h2>
       </div>
-      <div className='w-full bg-[#F3F4F6] h-[73%]  rounded-xl p-3'>
+      <div className='w-full bg-[#F3F4F6] h-[80%]  rounded-xl p-3'>
         <div>
           <p className='text-6xl m-5'> Runner Bolt</p>
         </div>
@@ -58,7 +58,7 @@ function App() {
               <div className='flex justify-between mt-5'>
 
                 <div className=''>
-                  <div className='text-3xl border-b-2 border-gray-400'>Size</div>
+                  <div className='text-4xl border-b-2 border-gray-400'>Size</div>
                   <div className='text-2xl mt-3 text-center'>
                     <p>5</p>
                     <p>6</p>
@@ -70,7 +70,7 @@ function App() {
                   </div>
                 </div>
                 <div className=''>
-                  <div className='text-3xl border-b-2 border-gray-400'>Availibility</div>
+                  <div className='text-4xl border-b-2 border-gray-400'>Availibility</div>
                   <div className='text-2xl mt-3 text-center'>
                     <p>75</p>
                     <p>106</p>
@@ -82,15 +82,15 @@ function App() {
                   </div>
                 </div>
                 <div className=''>
-                  <div className='text-3xl border-b-2 border-gray-400'>Color</div>
-                  <div className='text-2xl mt-3 text-center'>
-                    <p>5</p>
-                    <p>6</p>
-                    <p>7</p>
-                    <p>8</p>
-                    <p>9</p>
-                    <p>10</p>
-                    <p>11</p>
+                  <div className='text-4xl border-b-2 border-gray-400'>Color</div>
+                  <div className='text-2xl mt-3 text-center '>
+                    <p>Black</p>
+                    <p>Varsity Red</p>
+                    <p>white</p>
+                    <p>Ice Blue</p>
+                    <p>Light Steel Grey</p>
+                    <p>Treeline</p>
+                    <p>Canteen</p>
                   </div>
                 </div>
                 <div className='text-3xl border-b-2 '></div>
